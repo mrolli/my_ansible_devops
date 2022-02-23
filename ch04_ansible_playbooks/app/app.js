4fd50b1520042373a1b758b243e671083f013c1c
Michael Rolli <michael@rollis.ch>
2022-02-23 15:29:41 +0100

    Add nodejs playbook

---
+++ ch04_ansible_playbooks/app/app.js
@@ -0,0 +1,12 @@
+// Load the express module
+var express = require('express');
+var app = express();
+
+// Respond to requests for / with 'Hello World'
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// Listen on port 80 (lieka true web server)
+app.listen(80);
+console.log('Express server started successfully');
